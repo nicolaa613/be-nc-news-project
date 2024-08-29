98a4cfc85f3fa549b9bdee8ebdb4351068c73811
nicolaa613 <nicola.jones.94@btinternet.com>
2024-08-29 12:02:23 +0100

    add connection.js update

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,6 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
+const config = {};
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
@@ -9,11 +10,10 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const config = {};
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
