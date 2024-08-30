3e6d261450d3c59a92347ee2c59d4b03cc9833b0
nicolaa613 <nicola.jones.94@btinternet.com>
2024-08-30 15:40:20 +0100

    add completed task 8 including error handling

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { getArticle } = require("./controllers/get-article.controller");
 const { getAllArticles } = require("./controllers/get-all-articles.controller");
 const { getComments } = require("./controllers/get-comments.controller");
 const { postComment } = require("./controllers/post-comment.controller");
+const { patchArticle } = require("./controllers/patch-article.controller");
 
 app.use(express.json());
 
@@ -21,6 +22,8 @@ app.get("/api/articles/:article_id/comments", getComments);
 
 app.post("/api/articles/:article_id/comments", postComment);
 
+app.patch("/api/articles/:article_id", patchArticle);
+
 //Error-Handling Middleware
 
 app.all("/*", (request, response) => {
@@ -30,10 +33,24 @@ app.all("/*", (request, response) => {
 });
 
 app.use((error, request, response, next) => {
-  if (error.code === "23503") {
-    response.status(404).send({ message: "Username does not exist!" });
+  if (error.code === "23503" && error.detail.includes("article_id")) {
+    response.status(404).send({ message: "Cannot post comment, article doesn't exist!" });
+  } else if (error.code === "23503") {
+    response.status(404).send({ message: "Username does not exist!" }); 
   } else if (error.code === "23502") {
     response.status(400).send({ message: "No comment to post!" });
+  } else if (error.code === "22P02" && error.where.includes("unnamed portal parameter $3")) {
+    response
+      .status(400)
+      .send({ message: "This is a bad request, invalid article id!" });
+  } else if (error.code === "22P02" && error.where.includes("unnamed portal parameter $1")) {
+    response
+      .status(404)
+      .send({ message: "Cannot alter vote, article doesn't exist!" });
+  } else if (error.code === "22P02") {
+    response
+      .status(404)
+      .send({ message: "This is a bad request, invalid input!" });
   } else {
     next(error);
   }
